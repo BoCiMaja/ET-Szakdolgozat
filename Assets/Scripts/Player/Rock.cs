cea8b76e7a221b487b299161036457c2a0d8c12a
Shu <csaba19999@gmail.com>
2022-02-20 13:20:17 +0100

    Added rock trajectory

--- Assets/Scripts/Player/Rock.cs
+++ Assets/Scripts/Player/Rock.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    public Vector3 offset;
     public Vector3 direction;
+
     public float speed;
     public System.Action destroyed;
 
-
+    private void Start()
+    {
+        var adjustedThrow = direction + Vector3.up;
+        GetComponent<Rigidbody2D>().AddForce(adjustedThrow * speed, ForceMode2D.Impulse);
+        transform.Translate(offset);
+    }
     private void Update()
     {
-        RockTrajectory();
+        ThrowInALine();
     }
 
-    public void RockTrajectory()
+    public void ThrowInALine()
     {
-        this.transform.position += this.direction * this.speed * Time.deltaTime;
+        this.transform.position += direction * this.speed * Time.deltaTime;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy") || collision.CompareTag("Wall") || collision.CompareTag("Ground")
