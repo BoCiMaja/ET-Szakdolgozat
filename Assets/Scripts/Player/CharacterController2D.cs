32758a5ee2412b80901c530b477e7127e46cef79
Shu <csaba19999@gmail.com>
2022-03-11 10:17:58 +0100

    Switched Run and Walk

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -7,22 +7,15 @@ using UnityEngine.Audio;
 public class CharacterController2D : MonoBehaviour
 {
 	[SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
-	[Range(0, 1)] [SerializeField] private float m_WalkSpeed = .36f;
-	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
 	[SerializeField] public Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
-	[SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	public bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-	private Vector3 m_Velocity = Vector3.zero;
-	public bool floating;
 	public float jumpDelay = 0.5f;
 	public bool doubleJumpReady = false;
 	public CameraShake cameraShake;
@@ -34,12 +27,6 @@ public class CharacterController2D : MonoBehaviour
 
 	[System.Serializable]
 	public class BoolEvent : UnityEvent<bool> { }
-
-	public BoolEvent OnCrouchEvent;
-	private bool m_wasCrouching = false;
-
-	public BoolEvent OnWalkEvent;
-	private bool m_wasWalking = false;
 
 	public bool jump = false;
 	private int extraJump;
@@ -65,12 +52,6 @@ public class CharacterController2D : MonoBehaviour
 
 		if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
-
-		if (OnCrouchEvent == null)
-			OnCrouchEvent = new BoolEvent();
-
-		if (OnWalkEvent == null)
-			OnWalkEvent = new BoolEvent();
 	}
 
     private void Start()
@@ -81,10 +62,8 @@ public class CharacterController2D : MonoBehaviour
 	private void FixedUpdate()
 	{
 		jump = false;
-		Physics2D.gravity = new Vector2(0, -9.8f);
 		bool wasGrounded = m_Grounded;
 		m_Grounded = false;
-		floating = false;
 		animator.SetBool("isTurning", false);
 
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
@@ -101,30 +80,9 @@ public class CharacterController2D : MonoBehaviour
 				//StartCoroutine(cameraShake.Shaking());
 			}
 		}
-		/*if (!wasGrounded && Input.GetButton("Jump")) REPÜLÉS ANIMÁCIÓ
-		{
-			Physics2D.gravity = new Vector2(0,-0.8f);
-			floating = true;
-			animator.SetBool("isJumping", false);
-			animator.SetBool("isFloating", true);
-		}*/
-
-		Floating(); // Floating meghívva
 	}
 
-	public void Floating()
-    {
-		if (!m_Grounded && Input.GetButton("Jump") && m_Rigidbody2D.velocity.y < 0f &&
-			wallClimbing.isWall == false && wallClimbing.isClimbing == false) // floating, glide
-		{
-			Physics2D.gravity = new Vector2(0, -0.8f);
-			jump = false;
-			floating = true;
-			FindObjectOfType<SoundManager>().Pause("Running"); // running hang hivás
-			animator.SetBool("isJumping", false);
-			animator.SetBool("isFloating", true);
-		}
-	}
+	
 	private void Update()
     {
 		if (m_Grounded == true)
@@ -254,95 +212,6 @@ public class CharacterController2D : MonoBehaviour
 	void NoJump()
     {
 		doubleJumpReady = false;
-	}
-
-    public void Move(float move, bool jump, bool walk)
-	{
-		//if (!crouch)
-		//{
-		//	if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-		//	{
-		//		crouch = true;
-		//	}
-		//}
-		if (m_Grounded && jump)
-		{
-			m_Grounded = false;
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-		}
-		if (walk)
-        {
-			if (!m_wasWalking)
-            {
-				m_wasWalking = true;
-				OnWalkEvent.Invoke(true);
-			}
-			move *= m_WalkSpeed;
-		}
-		else
-        {
-			if (m_wasWalking)
-			{
-				m_wasWalking = false;
-				OnWalkEvent.Invoke(false);
-			}
-		}
-
-		if (m_Grounded || m_AirControl)
-		{
-			//if (crouch)
-			//{
-			//	if (!m_wasCrouching)
-			//	{
-			//		m_wasCrouching = true;
-			//		OnCrouchEvent.Invoke(true);
-			//	}
-			//	move *= m_CrouchSpeed;
-
-			//	if (m_CrouchDisableCollider != null)
-			//		m_CrouchDisableCollider.enabled = false;
-			//}
-			//else
-			//{
-			//	if (m_CrouchDisableCollider != null)
-			//		m_CrouchDisableCollider.enabled = true;
-
-			//	if (m_wasCrouching)
-			//	{
-			//		m_wasCrouching = false;
-			//		OnCrouchEvent.Invoke(false);
-			//	}
-			//}
-
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-			
-			//TODO: Lehetseges irrealis fizika miatt
-			//Vector3 smoothVelocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-			//Vector3 predictedPoint = transform.position + smoothVelocity * Time.fixedDeltaTime;
-
-			if (move > 0 && !m_FacingRight)
-			{
-				CreateDust(); // PARTICLE WHEN TURNING
-				Flip();
-				//animator.SetBool("isTurning", true);
-			}
-			else if (move < 0 && m_FacingRight)
-			{
-				CreateDust(); // PARTICLE WHEN TURNING
-				Flip();
-				//animator.SetBool("isTurning", false);
-			}
-		}
-	}
-
-	private void Flip()
-	{
-		m_FacingRight = !m_FacingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-		animator.SetBool("isTurning", true);
 	}
 
 	void CreateDust()
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private float JumpForce = 400f;
 
     public CharacterController2D controller;
+    private Rigidbody2D Rigidbody2D;
 
     public float runSpeed;
-
     float horizontalMove = 0f;
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+    private Vector3 velocity = Vector3.zero;
+
+    bool run = false;
+    bool walk = false;
 
     bool jump = false;
+    private int extraJump;
+    public int extraJumpValue;
+    [SerializeField] private bool airControl = false;
 
-    bool crouch = false;
+    private bool floating;
 
-    bool walk = false;
+    private bool m_FacingRight = true;
 
     public Animator animator;
 
     public ParticleSystem dust;
+
+    private void Awake()
+    {
+        extraJump = extraJumpValue;
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Rigidbody2D.interpolation = RigidbodyInterpolation2D.Interpolate;
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,43 +48,36 @@ public class PlayerMovement : MonoBehaviour
         horizontalMove =  Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        //if (Input.GetButtonDown("Crouch"))
-        //{
-        //    crouch = true;
-        //} else if (Input.GetButtonUp("Crouch"))
-        //{
-        //    crouch = false;
-        //}
-
-        if (Input.GetButtonDown("Run") && jump == false && walk == false)
+        if (Input.GetButtonDown("Walk") && jump == false && walk == false)
         {
-            animator.SetBool("isRunning", true);
-            runSpeed = 30f;
-            FindObjectOfType<SoundManager>().Stop("Walking"); 
-            FindObjectOfType<SoundManager>().Play("Running");
-        }
-        else if (Input.GetButtonUp("Run") || runSpeed == 0f){
-            animator.SetBool("isRunning", false);
-            FindObjectOfType<SoundManager>().Stop("Running");
-        }
-        if (Input.GetButtonDown("Walk") && Input.GetButton("Run"))
-        {
-            animator.SetBool("isRunning", false);
-            walk = true;
-            runSpeed = 20f;
+            animator.SetBool("isWalking", true);
+            runSpeed = 10f;
+            FindObjectOfType<SoundManager>().Stop("Running"); 
             FindObjectOfType<SoundManager>().Play("Walking");
-            FindObjectOfType<SoundManager>().Stop("Running");
+        }
+        else if (Input.GetButtonUp("Walk") || runSpeed == 0f){
+            animator.SetBool("isWalking", false);
+            FindObjectOfType<SoundManager>().Stop("Walking");
+        }
+        if (Input.GetButtonDown("Run") && Input.GetButton("Walk"))
+        {
+            animator.SetBool("isWalking", false);
+            walk = false;
+            runSpeed = 30f;
+            FindObjectOfType<SoundManager>().Play("Running");
+            FindObjectOfType<SoundManager>().Stop("Walking");
 
         }
-        else if (Input.GetButtonUp("Walk"))
+        else if (Input.GetButtonUp("Run"))
         {
-            walk = false;
-            FindObjectOfType<SoundManager>().Stop("Walking");
-            if(runSpeed > 0f && Input.GetButton("Run"))
+            run = false;
+            FindObjectOfType<SoundManager>().Stop("Running");
+            if(runSpeed > 0f && Input.GetButton("Walk"))
             {
-                animator.SetBool("isRunning", true);
-                runSpeed = 30f;
-                FindObjectOfType<SoundManager>().Play("Running");
+                walk = true;
+                animator.SetBool("isWalking", true);
+                runSpeed = 10f;
+                FindObjectOfType<SoundManager>().Play("Walking");
             }
         }
 
@@ -72,8 +85,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // MOVE
-        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, walk);
+        Physics2D.gravity = new Vector2(0, -9.8f);
+        floating = false;
+        Move(horizontalMove * Time.fixedDeltaTime, jump);
+        Turning();
+        Floating();
     }
 
     public void OnLanding()
@@ -82,26 +98,69 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("isJumping", false);
         animator.SetBool("jumpedAlready", false);
         animator.SetBool("isFloating", false);
-        if (runSpeed > 0f && Input.GetButton("Run"))
+        if (runSpeed > 0f && Input.GetButton("Walk"))
         {
-            animator.SetBool("isRunning", true);
-            runSpeed = 30f;
-            FindObjectOfType<SoundManager>().Play("Running");
+            animator.SetBool("isWalking", true);
+            runSpeed = 10f;
+            FindObjectOfType<SoundManager>().Play("Walking");
         }
     }
 
-    //public void OnCrouching(bool isCrouching)
-    //{
-    //    animator.SetBool("isCrouching", isCrouching);
-    //    FindObjectOfType<SoundManager>().Play("Crouching"); //crouch hang hivas
-    //    FindObjectOfType<SoundManager>().Stop("Running"); //crouch hang hivas
-    //}
-
-    public void OnWalking(bool isWalking)
+    public void Move(float move, bool jump)
     {
-        animator.SetBool("isWalking", isWalking);
+        if (controller.m_Grounded && jump)
+        {
+            controller.m_Grounded = false;
+            Rigidbody2D.AddForce(new Vector2(0f, JumpForce));
+        }
+
+        if (controller.m_Grounded || airControl)
+        {
+            Vector3 targetVelocity = new Vector2(move * 10f, Rigidbody2D.velocity.y);
+            Rigidbody2D.velocity = Vector3.SmoothDamp(Rigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
+        }
     }
 
+
+    private void Turning()
+    {
+        if (horizontalMove > 0 && !m_FacingRight)
+        {
+            CreateDust(); // PARTICLE WHEN TURNING
+            Flip();
+            //animator.SetBool("isTurning", true);
+        }
+        else if (horizontalMove < 0 && m_FacingRight)
+        {
+            CreateDust(); // PARTICLE WHEN TURNING
+            Flip();
+            //animator.SetBool("isTurning", false);
+        }
+    }
+
+    public void Floating()
+    {
+        if (!controller.m_Grounded && Input.GetButton("Jump") && Rigidbody2D.velocity.y < 0f &&
+            controller.wallClimbing.isWall == false && controller.wallClimbing.isClimbing == false) // floating, glide
+        {
+            Physics2D.gravity = new Vector2(0, -0.8f);
+            jump = false;
+            floating = true;
+            FindObjectOfType<SoundManager>().Stop("Running"); // running hang hivás
+            FindObjectOfType<SoundManager>().Stop("Walking"); // running hang hivás
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isFloating", true);
+        }
+    }
+
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        animator.SetBool("isTurning", true);
+    }
 
     void CreateDust()
     {
