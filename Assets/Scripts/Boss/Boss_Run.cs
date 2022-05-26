b32e372b25399586f79f7c21102c55e4123364ea
Shu <csaba19999@gmail.com>
2022-05-26 14:49:07 +0200

    Added boss movement and dialogue anim

--- Assets/Scripts/Boss/Boss_Run.cs
+++ Assets/Scripts/Boss/Boss_Run.cs
@@ -4,39 +4,63 @@ using UnityEngine;
 
 public class Boss_Run : StateMachineBehaviour
 {
-    public float speed = 2.5f;
+    public float speed = 1;
     public float attackRange = 3f;
+    public Vector3 offset;
+    public Vector3 direction;
+    public Vector3 adjustedThrow;
+
+    public int lifeTime = 5;
+    public GameObject gameObject;
+
 
     Transform player;
+    GameObject player2;
     Rigidbody2D rb;
     Boss boss;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        player =  GameObject.FindGameObjectWithTag("Player").transform;
-       rb = animator.GetComponent<Rigidbody2D>();
+
+        rb = animator.GetComponent<Rigidbody2D>();
         boss = animator.GetComponent<Boss>();
+
+        adjustedThrow = direction;
+        rb.AddForce(adjustedThrow * speed, ForceMode2D.Impulse);
+        boss.transform.Translate(offset);
+        Destroy(GameObject.Find("Boss"), lifeTime);
     }
 
-// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public void MoveInALine()
     {
-        boss.LookAtPlayer();
+        boss.transform.position += direction * this.speed * Time.deltaTime;
+    }
 
-        Vector2 targetPlayer = new Vector2(player.position.x, rb.position.y);
-        Vector2 newBossPos =  Vector2.MoveTowards(rb.position, targetPlayer, speed * Time.fixedDeltaTime);
-        rb.MovePosition(newBossPos);
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //gameObject.transform.position += gameObject.transform.forward * Time.deltaTime * speed;
+        //rb.MovePosition(gameObject.transform.position + speed * Time.deltaTime * direction);
 
-        if(Vector2.Distance(player.position, rb.position)<= attackRange) {
-            animator.SetTrigger("Attack");
-        }
+        MoveInALine();
+
+        //boss.LookAtPlayer();
+
+        //Vector2 targetPlayer = new Vector2(player.position.x, rb.position.y);
+        //Vector2 newBossPos =  Vector2.MoveTowards(rb.position*-1, targetPlayer, speed * Time.fixedDeltaTime);
+        //rb.MovePosition(newBossPos);
+
+        //if(Vector2.Distance(player.position, rb.position)<= attackRange) {
+        //    animator.SetTrigger("Attack");
+        //}
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.ResetTrigger("Attack");
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    animator.ResetTrigger("Attack");
+    //}
 
 
 }
--- Assets/Scripts/Boss/Bossfight_End.cs
+++ Assets/Scripts/Boss/Bossfight_End.cs
@@ -6,10 +6,19 @@ public class Bossfight_End : StateMachineBehaviour
 {
     public int lifeTime = 10;
     public GameObject gameObject;
+    public Animator animator;
+    public float dialogue;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Destroy(GameObject.Find("Boss"), lifeTime);
+        Destroy(GameObject.Find("BossText"), lifeTime);
+        dialogue = 10;
+        animator.SetInteger("Dialogue", ((int)dialogue));
     }
 
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        dialogue -= Time.deltaTime;
+        animator.SetInteger("Dialogue", ((int)dialogue));
+    }
 }
